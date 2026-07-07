e76542032bcfabb9d3227a2772505591267ccd4e
Vennila200405 <vennilas2004@gmail.com>
2026-07-07 11:50:00 +0530

    Create testbench.v

---
+++ odd_even_detector/testbench.v
@@ -0,0 +1,47 @@
+`timescale 1ns/1ps
+
+module odd_even_detector_tb;
+reg [3:0]a;
+wire odd;
+wire even;
+
+odd_even_detector uut (
+                       .a(a),
+                       .odd(odd),
+                       .even(even)
+);
+
+  initial begin
+
+    $dumpfile("dump.vcd");
+    $dumpvars(0,odd_even_detector_tb);
+
+    $display("Time\tInput\tOdd\tEven");
+    $monitor("%0t\t%b\t%b\t%b", $time, a, odd, even);
+
+end
+
+  initial begin
+
+    a = 4'b0000; #10;
+    a = 4'b0001; #10;
+    a = 4'b0010; #10;
+    a = 4'b0011; #10;
+    a = 4'b0100; #10;
+    a = 4'b0101; #10;
+    a = 4'b0110; #10;
+    a = 4'b0111; #10;
+    a = 4'b1000; #10;
+    a = 4'b1001; #10;
+    a = 4'b1010; #10;
+    a = 4'b1011; #10;
+    a = 4'b1100; #10;
+    a = 4'b1101; #10;
+    a = 4'b1110; #10;
+    a = 4'b1111; #10;
+
+    $finish;
+end
+
+endmodule
+ 
