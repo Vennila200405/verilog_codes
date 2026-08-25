2a43470abcdc39d113ae5fdb43f34e7c9d73a51c
Vennila200405 <vennilas2004@gmail.com>
2026-08-25 16:58:06 +0530

    Add testbench for full adder module

---
+++ Combinational_circuits/Full_adder/testbench.v
@@ -0,0 +1,19 @@
+module full_adder_tb;
+reg a,b,c;
+wire sum,carry;
+  full_adder DUT(a,b,c,sum,carry);
+initial begin
+  $dumpfile("dump.vcd");
+    $dumpvars();
+  $monitor("a=%b b=%b c=%b sum=%b carry=%b", a, b,c, sum,carry);
+        a=0; b=0;c=0;
+    #10 a=0; b=0;c=1;
+    #10 a=0; b=1;c=0;
+    #10 a=0; b=1;c=1;
+    #10 a=1; b=0;c=0;
+    #10 a=1; b=0;c=1;
+    #10 a=1; b=1;c=0;
+    #10 a=1; b=1;c=1;
+    $finish;
+  end
+endmodule
