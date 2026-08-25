0ae147ea730a150df12d51cc2646209a73cd4465
Vennila200405 <vennilas2004@gmail.com>
2026-08-25 16:18:01 +0530

    Add testbench for half subtractor module

---
+++ half_subtractor/testbench.v
@@ -0,0 +1,15 @@
+module half_subtractor_tb;
+reg a, b;
+wire diff, borrow;
+half_subtractor DUT (a, b, diff, borrow);
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, half_subtractor_tb);
+$monitor("a=%b b=%b diff=%b borrow=%b", a, b, diff, borrow);
+a = 0; b = 0; #10;
+a = 0; b = 1; #10;
+a = 1; b = 0; #10;
+a = 1; b = 1; #10;
+$finish;
+  end
+endmodule
