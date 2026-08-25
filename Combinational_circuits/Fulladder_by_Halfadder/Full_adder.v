608726d5a82a22526c2549dc7a05e1affeab529c
Vennila200405 <vennilas2004@gmail.com>
2026-08-25 17:31:50 +0530

    Create Full_adder.v

---
+++ Combinational_circuits/Fulladder_by_Halfadder/Full_adder.v
@@ -0,0 +1,27 @@
+module Full_Adder(
+input a,b,cin,
+output sum,
+output reg cout
+);
+
+wire s1,c1,c2;
+
+Half_Adder HA1(
+           .a(a),
+           .b(b),
+           .sum(s1),
+           .carry(c1)
+);
+
+Half_Adder  HA2(
+           .a(s1),
+           .b(cin),
+           .sum(sum),
+           .carry(c2)
+);
+
+always @(*)begin
+
+cout=c1|c2;
+end
+endmodule
