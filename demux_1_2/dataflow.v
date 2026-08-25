9ac2be287e7c0d6541dbaea273d8506d4e84c049
Vennila200405 <vennilas2004@gmail.com>
2026-08-25 21:20:04 +0530

    Delete demux_1_2 directory

--- demux_1_2/dataflow.v
+++
@@ -1,7 +0,0 @@
-module demux_1_2(
-  input sel,
-  input i,
-  output y0,y1);
-  assign y0=(~sel)&i;
-  assign y1=sel&i;
-endmodule
--- demux_1_2/gatelevel.v
+++
@@ -1,8 +0,0 @@
-module demux1_2(i, s, y0, y1);
-input i,s;
-output y0, y1;
-wire ns;
-not (ns, s);
-and (y0, i, ns);
-and (y1, i, s);
-endmodule
