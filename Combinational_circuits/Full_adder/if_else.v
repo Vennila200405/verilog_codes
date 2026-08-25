2d0fc6460418d684e74c656d8e45a54252e9d237
Vennila200405 <vennilas2004@gmail.com>
2026-08-25 16:56:39 +0530

    Create if_else.v

---
+++ Combinational_circuits/Full_adder/if_else.v
@@ -0,0 +1,31 @@
+module full_adder(
+input a,b,c,
+output reg sum,carry
+);
+always @(*)begin
+if(a==0 && b==0 && c==0)begin
+sum=0;carry=0;
+end
+else if(a==0 && b==0 && c==1)begin
+sum=1;carry=0;
+end
+else if(a==0 && b==1 && c==0)begin
+sum=1;carry=0;
+end
+else if(a==0 && b==1 && c==1)begin
+sum=0;carry=1;
+end
+else if(a==1 && b==0 && c==0)begin
+sum=1;carry=0;
+end
+else if(a==1 && b==0 && c==1)begin
+sum=0;carry=1;
+end
+else if(a==1 && b==1 && c==0)begin
+sum=0;carry=1;
+end
+else begin
+sum=1;carry=1;
+end
+end 
+endmodule
