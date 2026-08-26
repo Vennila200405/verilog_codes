166ef2c8baa45ceafee449c3380dfefd357f423c
Vennila200405 <vennilas2004@gmail.com>
2026-08-26 21:54:55 +0530

    Create testbench.v

---
+++ FSM/Mealy/non_overlapping_1001/testbench.v
@@ -0,0 +1,30 @@
+module mealy_no_tb;
+reg rst, clk, x;
+wire y;
+
+mealy_no DUT (clk, rst, x, y);
+always #5 clk = ~clk;
+initial begin
+    clk = 0;
+    rst = 1;
+    x = 0;
+
+    $dumpfile("dump.vcd");
+    $dumpvars(0, mealy_no_tb);
+
+    $monitor("time=%0t clk=%b rst=%b x=%b y=%b", $time, clk, rst, x, y);
+    @(posedge clk);
+    rst = 0;
+    @(posedge clk) x = 1;
+    @(posedge clk) x = 0;
+    @(posedge clk) x = 0;
+    @(posedge clk) x = 1;
+
+    @(posedge clk) x = 0;
+    @(posedge clk) x = 0;
+    @(posedge clk) x = 1;
+    @(posedge clk);
+    $finish;
+end
+
+endmodule
